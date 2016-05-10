c60fe6cd2e66bd5f0b42989a24a213ad4a622a94
gnishida <gnishida@purdue.edu>
2016-05-10 12:24:03 -0400

    update.

--- RollingGuidanceFilter_Matlab/example.m
+++ RollingGuidanceFilter_Matlab/example.m
@@ -2,11 +2,11 @@
 close all;
 
 %I = im2double(imread('image.png'));
-I = im2double(imread('building4.png'));
+I = im2double(imread('1_2.png'));
 
 tic;
 %res = RollingGuidanceFilter(I,3,0.05,4);
-res = RollingGuidanceFilter(I,40,0.05,10);
+res = RollingGuidanceFilter(I,4,0.05,8);
 toc;
 
 figure,imshow(I);
